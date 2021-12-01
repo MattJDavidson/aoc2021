fc6f3456712da39667a107a89eecd7b44992587c
Matt Davidson <matt@mattjdavidson.com>
2021-12-01 19:22:07 +0200

    pyrival

---
+++ pyrival/__init__.py
@@ -0,0 +1,6 @@
+import os as _os
+
+
+for _s in ('algebra', 'combinatorics', 'data_structures', 'geometry', 'graphs', 'linear_algebra', 'numerical',
+           'strings', 'misc', 'tools'):
+    __path__.append(_os.path.join(_os.path.dirname(__file__), _s))
